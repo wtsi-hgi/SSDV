a8d8e4e0881daf64161ae656f4ebe9960dd67d36
mo11 <mo11@sanger.ac.uk>
2021-02-23 00:26:56 +0000

    production

--- backend/settings.py
+++ backend/settings.py
@@ -15,8 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-print(BASE_DIR)
-print("hello!!!!")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
