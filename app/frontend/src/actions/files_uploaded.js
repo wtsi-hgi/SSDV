c6beb088326847235cd1d32beeac9ec13e54bc5f
maxozo <mo11@sanger.ac.uk>
2021-02-25 13:23:12 +0000

    have to put the subpath in the react subdirectory

--- app/frontend/src/actions/files_uploaded.js
+++ app/frontend/src/actions/files_uploaded.js
@@ -4,8 +4,8 @@ import {GET_FILES, DELETE_FILES} from "./types";
 
 //Get Files
 export const getFiles = () => (dispatch, getState) => {
-    alert("getting")
-    axios.get('/api_scrna/files/')
+    // alert("getting 2")
+    axios.get('/scrna/api_scrna/files/')
         .then(res => {
         dispatch({
             type: GET_FILES,
@@ -15,10 +15,10 @@ export const getFiles = () => (dispatch, getState) => {
     }).catch(err => console.log(err));
 }
 
-// Delet files
+// Delete files
 export const deleteFiles = (id) => (dispatch, getState) => {
     
-    axios.delete(`/api_scrna/files/${id}`)
+    axios.delete(`/scrna/api_scrna/files/${id}`)
         .then(res => {
         dispatch({
             type: DELETE_FILES,
