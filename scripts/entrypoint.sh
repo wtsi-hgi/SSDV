30cbfef0dd66d733b1b0ea249168d974abb59076
maxozo <mo11@sanger.ac.uk>
2021-02-25 18:12:08 +0000

    all working, works in django production mode and on the test swarm

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,6 +3,9 @@
 set -e
 
 python manage.py collectstatic --noinput
+echo "Apply database migrations"
+python manage.py makemigrations
+python manage.py migrate
 uwsgi --socket :8000 --master --enable-threads --static-map /static=/vol/static --module backend.wsgi
 
 # uwsgi --socket subdirectories.sock --master --enable-threads --module backend.wsgi
