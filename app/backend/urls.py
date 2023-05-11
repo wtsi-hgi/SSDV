ae1e486b4ef2d37f7232ef1c0897dd458b9774da
maxozo <mo11@sanger.ac.uk>
2023-05-11 14:14:43 +0000

    some changes

--- app/backend/urls.py
+++ app/backend/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api_scrna/',include('browserApp.urls')),
-    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico')))
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('frontend/favicon.ico')))
     
     
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
